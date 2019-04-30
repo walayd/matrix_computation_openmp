56e19ca9081bc23ee7a8c54421b5244cfb6fa5a2
Walid Larabi <walid.larabi@etu.unice.fr>
2019-04-30 13:56:38 +0200

    reading arguments

--- source.c
+++ source.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char* argv[])
 {
+   if (argc < 3){
+     printf("Error, arguments missing\n");	
+     return 1;
+   }
 
-
-   return 0;
+   printf("first arg : %s\nsecond arg : %s\n", argv[1], argv[2]);
+   return EXIT_SUCCESS;
 }
+
+
+
